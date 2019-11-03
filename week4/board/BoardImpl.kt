a2cef33ade4e967ed035270869413f5be847938b
IgorOzherelev <43535311+IgorOzherelev@users.noreply.github.com>
2019-11-03 15:03:10 +0200

    4 week

---
+++ week4/board/BoardImpl.kt
@@ -0,0 +1,70 @@
+package week4.board
+
+import board.GameBoardImpl
+import board.SquareBoardImpl
+import week4.board.Direction.*
+
+fun createSquareBoard(width: Int): SquareBoard = SquareBoardImpl(width)
+fun <T> createGameBoard(width: Int): GameBoard<T> = GameBoardImpl(width)
+
+
+class GameBoardImpl<T>(width: Int) : SquareBoardImpl(width), GameBoard<T> {
+    private val data: MutableMap<Cell, T?> = hashMapOf()
+
+    init {
+        getAllCells().forEach { set(it, null)}
+    }
+
+    override fun get(cell: Cell): T? = data[cell]
+
+    override fun set(cell: Cell, value: T?) {
+        data[cell] = value
+    }
+
+    override fun filter(predicate: (T?) -> Boolean): Collection<Cell> =
+            data.filter { predicate(it.value) }.keys
+
+
+    override fun find(predicate: (T?) -> Boolean): Cell? =
+            filter(predicate).firstOrNull()
+
+    override fun any(predicate: (T?) -> Boolean): Boolean =
+            !filter(predicate).isEmpty()
+
+    override fun all(predicate: (T?) -> Boolean): Boolean =
+            filter(predicate).size == data.size
+}
+
+
+open class SquareBoardImpl(override val width: Int) : SquareBoard {
+    private val cells = (1..width).flatMap { i -> (1..width).map{ Cell(i, it) } }
+
+    override fun getCellOrNull(i: Int, j: Int): Cell? =
+            cells.firstOrNull { it.i == i && it.j == j }
+
+
+    override fun getCell(i: Int, j: Int): Cell = getCellOrNull(i, j)!!
+
+    override fun getAllCells(): Collection<Cell> = cells
+
+    override fun getRow(i: Int, jRange: IntProgression): List<Cell> {
+        val row = cells.filter{ it.i == i }
+
+        return jRange.takeWhile { it <= width }.map { row.find { cell -> cell.j == it } as Cell }
+    }
+
+    override fun getColumn(iRange: IntProgression, j: Int): List<Cell> {
+        val column = cells.filter{ it.j == j }
+
+        return iRange.takeWhile { it <= width }.map{ column.find{ cell -> cell.i == it } as Cell }
+    }
+
+    override fun Cell.getNeighbour(direction: Direction): Cell? =
+            when (direction){
+                DOWN -> getCellOrNull(i + 1, j)
+                UP -> getCellOrNull(i - 1, j)
+                LEFT -> getCellOrNull(i, j - 1)
+                RIGHT -> getCellOrNull(i, j + 1)
+            }
+
+}
